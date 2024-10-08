e1abe8c31e7cfb686da825857c813592a3400303
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-10-07 21:24:32 -0300

    chore: eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import { app } from "./app";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({ port: 3333, host: "0.0.0.0" }, () => {
-  console.log("🚀 Server is running on port 3333");
-});
+app.listen({ port: env.PORT, host: '0.0.0.0' }, () => {
+  console.log(`🚀 Server is running on port ${env.PORT}`)
+})

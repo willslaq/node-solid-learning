6b8de2bd95f13f48b0342a85d678c87b1e70a1e6
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-10-18 09:23:58 -0300

    feat(check-in): added checkins on same day validation

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,20 +1,28 @@
 import { Checkin, Prisma } from '@prisma/client'
 import { CheckInsRepository } from '../check-ins.repository'
 import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckinsRepository implements CheckInsRepository {
   public items: Checkin[] = []
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const checkOnSameDate = this.items.find(
-      (checkIn) => checkIn.user_id === userId,
-    )
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
 
-    if (!checkOnSameDate) {
+    const checkInOnSameDate = this.items.find((checkin) => {
+      const checkInDate = dayjs(checkin.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkin.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
       return null
     }
 
-    return checkOnSameDate
+    return checkInOnSameDate
   }
 
   async create(data: Prisma.CheckinUncheckedCreateInput) {

0db852d6b29da164ce0d353efbd5cc47262df36c
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-10-26 15:39:22 -0300

    feat: added authenticate with jwt

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -22,7 +22,18 @@ export async function authenticateController(
       password,
     })
 
-    return reply.status(200).send({ user })
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register.controller'
 import { authenticateController } from './controllers/authenticate.controller'
+import { profileController } from './controllers/profile.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
 
   app.post('/sessions', authenticateController)
+
+  app.get('/me', profileController)
 }

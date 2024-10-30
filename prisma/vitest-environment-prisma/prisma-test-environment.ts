11f392dd448d781490797811df3aea8c94a3baf5
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-10-29 22:43:06 -0300

    test: start configuration of the test-environment db

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,5 +1,23 @@
+import { randomUUID } from 'node:crypto'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
 export default {
   async setup() {
+    const schema = randomUUID()
+
+    console.log(generateDatabaseURL(schema))
+
     console.log('Setting up Prisma Test Environment...')
     return {
       teardown() {

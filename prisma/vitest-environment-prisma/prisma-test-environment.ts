d0730b63cfcf9eee9f038c95f969ed86db910122
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-10-30 20:50:52 -0300

    test: finished the tests config and made first e2e test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,16 @@
 import { randomUUID } from 'node:crypto'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
     throw new Error('Please provide a DATABASE_URL environment variable.')
   }
-
   const url = new URL(process.env.DATABASE_URL)
-
   url.searchParams.set('schema', schema)
-
   return url.toString()
 }
 
@@ -16,20 +18,15 @@ export default {
   async setup() {
     const schema = randomUUID()
 
-    console.log(generateDatabaseURL(schema))
+    process.env.DATABASE_URL = generateDatabaseURL(schema)
 
-    console.log('Setting up Prisma Test Environment...')
+    execSync('npx prisma migrate deploy')
     return {
-      teardown() {
-        console.log('Tearing down Prisma Test Environment...')
-      },
-    }
-  },
-  async setupVM() {
-    console.log('Setting up Prisma VM Test Environment...')
-    return {
-      teardown() {
-        console.log('Tearing down Prisma VM Test Environment...')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,23 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {
-  expect(1).toBe(1)
+describe('Register Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'WzXZ3@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
